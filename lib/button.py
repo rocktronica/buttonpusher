a56006e2b5037ee0cb4ee37827eab5c2252e6b61
Tommy <tommy@iamnotagoodartist.com>
2020-05-16 14:40:38 -0700

    Extract Button

---
+++ lib/button.py
@@ -0,0 +1,20 @@
+import digitalio
+
+class Button():
+    def __init__(self, pin):
+        self._button = digitalio.DigitalInOut(pin)
+        self._button.direction = digitalio.Direction.INPUT
+        self._button.pull = digitalio.Pull.DOWN
+
+        self._value = False
+
+    def reset(self):
+        self._value = False
+
+    def is_pressed(self):
+        if self._value and self._button.value:
+            return True
+        elif not self._value and not self._button.value:
+            self._value = True
+
+        return False
--- main.py
+++ main.py
@@ -4,26 +4,8 @@ import pulseio
 from adafruit_motor import servo
 from functools import reduce
 import rotaryio
-import digitalio
 
-class Button():
-    def __init__(self, pin):
-        self._button = digitalio.DigitalInOut(pin)
-        self._button.direction = digitalio.Direction.INPUT
-        self._button.pull = digitalio.Pull.DOWN
-
-        self._value = False
-
-    def reset(self):
-        self._value = False
-
-    def is_pressed(self):
-        if self._value and self._button.value:
-            return True
-        elif not self._value and not self._button.value:
-            self._value = True
-
-        return False
+from button import Button
 
 class Wait():
     def __init__(self, cancel_button):
