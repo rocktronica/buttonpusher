81498434c33ff99b080ec7552b8075ed85138f7c
Tommy <tommy@iamnotagoodartist.com>
2020-05-16 14:49:46 -0700

    Extract sequences

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from functools import reduce
 import rotaryio
 
 from button import Button
+from sequences import SEQUENCES
 
 class Wait():
     def __init__(self, cancel_button):
@@ -229,78 +230,6 @@ def run(sequence = [], count = 0):
 
     hammer.default()
     wait.sleep(1)
-
-REDEEM_NOOK_MILES_SEQUENCE = [
-    {
-        "seconds": 2.5,
-        "description": 'Wait for menu and select item',
-    },
-    {
-        "seconds": 3,
-        "description": 'Redeem ...?',
-    },
-    {
-        "seconds": .75,
-        "description": 'Confirm',
-    },
-    {
-        "seconds": 8,
-        "description": 'Wait for item and receive',
-    },
-    {
-        "seconds": 2.5,
-        "description": 'click through explanation',
-    },
-    {
-        "seconds": 4.5,
-        "description": 'put item away and make another selection',
-    },
-    {
-        "seconds": .75,
-        "description": 'confirm dialog',
-    },
-]
-CRAFT_SEQUENCE = [
-    {
-        "seconds": 3,
-        "description": 'Wait for menu and select item'
-    },
-    {
-        "seconds": .75,
-        "description": 'Craft it!'
-    },
-    {
-        "seconds": .75,
-        "description": 'Confirm'
-    },
-    {
-        "seconds": 7,
-        "description": 'Make item and receive it'
-    },
-    {
-        "seconds": .75,
-        "description": 'Keep crafting'
-    },
-]
-WISH_ON_A_STAR_SEQUENCE = [
-    {
-        "seconds": 1.5,
-        "description": 'Wish on a possible star'
-    },
-]
-DEBUG_SEQUENCE = [
-    {
-        "seconds": 1,
-        "description": 'Debug'
-    },
-]
-
-SEQUENCES = [
-    {"text": "Redeem Nook Miles", "value": REDEEM_NOOK_MILES_SEQUENCE},
-    {"text": "Craft item", "value": CRAFT_SEQUENCE},
-    {"text": "Wish on a star", "value": WISH_ON_A_STAR_SEQUENCE},
-    {"text": "Debug", "value": DEBUG_SEQUENCE},
-]
 
 while True:
     hammer.default()
