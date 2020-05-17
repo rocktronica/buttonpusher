734c5edcec4ef9810a9d061e0a511a7cf1ba28e9
Tommy <tommy@iamnotagoodartist.com>
2020-05-16 18:03:01 -0700

    Add 3rd party libs

---
+++ lib/functools.py
@@ -0,0 +1,9 @@
+def reduce(function, iterable, initializer=None):
+    it = iter(iterable)
+    if initializer is None:
+        value = next(it)
+    else:
+        value = initializer
+    for element in it:
+        value = function(value, element)
+    return value
