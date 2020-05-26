7092927134f956eb7a6f1e8e3ccf00e049f86bb2
Tommy <tommy@iamnotagoodartist.com>
2020-05-25 18:34:33 -0700

    Slow 'er down there, honcho
    Was seeing some skipped beats, probably from hammer timing updates

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,19 +1,19 @@
 SEQUENCES = [
 	{"text": "Craft Item", "value": [
 		3, # Wait for menu and select item
-		.75, # Craft it!
-		.75, # Confirm
+		1, # Craft it!
+		1, # Confirm
 		7, # Make item and receive it
-		.75, # Keep crafting
+		1, # Keep crafting
 	]},
 	{"text": "Bell Voucher", "value": [
 		2.5, # Wait for menu and select item
 		3, # Redeem ...?
-		.75, # Confirm
+		1, # Confirm
 		8, # Wait for item and receive
 		2.5, # click through explanation
 		4.5, # put item away and make another selection
-		.75, # confirm dialog
+		1, # confirm dialog
 	]},
 	{"text": "Wish on Stars", "value": [
 		1.5, # Wish on a possible star
